5fbfe01a298426d91bdd9b8bfd3650cf04c19b90
Rhys Mainwaring <rhys.mainwaring@me.com>
2022-04-01 15:47:14 +0100

    Workarounds to support macOS (Big Sur 11.6.2)
    - Missing endian conversion functions
    - Workaround issues with RCLCPP_SMART_PTR_DEFINITIONS macros and abstract base classes
    - Link issues for mavros_extras_plugins (still one remaining link issue to resolve...)
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- libmavconn/include/mavconn/interface.hpp
+++ libmavconn/include/mavconn/interface.hpp
@@ -23,6 +23,38 @@
 #ifndef MAVCONN__INTERFACE_HPP_
 #define MAVCONN__INTERFACE_HPP_
 
+/////////////////////////////////////////////////
+// https://debugjournal.tumblr.com/post/99661010362/missing-byteswaph-and-endianh-on-mac-os-x
+#ifdef __APPLE__
+#include <machine/endian.h>
+#include <libkern/OSByteOrder.h>
+
+#define htobe16(x) OSSwapHostToBigInt16(x)
+#define htole16(x) OSSwapHostToLittleInt16(x)
+#define be16toh(x) OSSwapBigToHostInt16(x)
+#define le16toh(x) OSSwapLittleToHostInt16(x)
+
+#define htobe32(x) OSSwapHostToBigInt32(x)
+#define htole32(x) OSSwapHostToLittleInt32(x)
+#define be32toh(x) OSSwapBigToHostInt32(x)
+#define le32toh(x) OSSwapLittleToHostInt32(x)
+
+#define htobe64(x) OSSwapHostToBigInt64(x)
+#define htole64(x) OSSwapHostToLittleInt64(x)
+#define be64toh(x) OSSwapBigToHostInt64(x)
+#define le64toh(x) OSSwapLittleToHostInt64(x)
+
+#define __BIG_ENDIAN    BIG_ENDIAN
+#define __LITTLE_ENDIAN LITTLE_ENDIAN
+#define __BYTE_ORDER    BYTE_ORDER
+#else
+#include 
+#include 
+#endif
+/////////////////////////////////////////////////
+
+#include <mavconn/mavlink_dialect.hpp>
+
 #include <atomic>
 #include <cassert>
 #include <chrono>
